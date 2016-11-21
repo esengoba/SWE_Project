0498183770190fd3dad25169739902dd87079628
Joshlix95 <joshlixmus@gmail.com>
2016-11-20 23:14:19 -0500

    Groundwork for issue #46

--- src/model/Leaderboard.java
+++ src/model/Leaderboard.java
@@ -1,5 +1,24 @@
 package model;
 
-
 public class Leaderboard {
+
+    /**
+     * This function takes the ID and score of the player, and places it in
+     * the respective file if the score is among the top ten highest scores.
+     * @param userID
+     * @param score
+     */
+    public void addScore(String userID, String score){
+
+    }
+
+    /**
+     * This function checks if a given score is among the top ten in a file
+     * of users' scores. This is a helper method to the addScore function.
+     * @param score
+     * @return
+     */
+    public boolean isTopTen(String score){
+        return false;
+    }
 }
